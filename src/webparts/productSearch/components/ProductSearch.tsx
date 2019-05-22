0992a4323070e4f8a48ca409a48a6f0161083019
Corey Roth <coreyroth@Corey-Roths-Work-MacBook-Pro.local>
2019-05-22 12:04:41 -0700

    Added new list view for products list.

--- src/webparts/productSearch/components/ProductSearch.tsx
+++ src/webparts/productSearch/components/ProductSearch.tsx
@@ -8,33 +8,46 @@ import { DetailsList, DetailsListLayoutMode, Selection, SelectionMode, IColumn }
 
 
 export default class ProductSearch extends React.Component<IProductSearchProps, {
-  query: string;
-  searchResults: SearchResult[]
+  query: string,
+  searchResults: SearchResult[],
+  loading: boolean
 }> {
+  private _columns: IColumn[];
+
   constructor(props: IProductSearchProps) {
     super(props);
     this.state = {
         query: undefined,
-        searchResults: undefined
+        searchResults: undefined,
+        loading: false
     };
 }
   public render(): React.ReactElement<IProductSearchProps> {
+    this._columns = [
+      { key: 'title', name: 'Title', fieldName: 'Title', minWidth: 100, maxWidth: 200, isResizable: true },
+      { key: 'author', name: 'Author', fieldName: 'Author', minWidth: 100, maxWidth: 200, isResizable: true },
+      { key: 'write', name: 'Write', fieldName: 'Write', minWidth: 100, maxWidth: 200, isResizable: true }
+    ];
     return (
       <div className={ styles.productSearch }>
-        <SearchBox value={this.state.query} onSearch={this._pnSearch}
+        <SearchBox value={this.state.query} onSearch={this._onSearch}
           onChange={newValue => {
             this.setState({
               query: newValue
             });
           }}
           />
+          {!this.state.loading && this.state.searchResults &&
+            <DetailsList items={this.state.searchResults} columns={this._columns} >
+            </DetailsList>
+          }
       </div>
     );
   }
 
-  public _pnSearch = async (): Promise<void> => {
+  public _onSearch = async (): Promise<void> => {
     let listUrl: string = 'https://m365x301749.sharepoint.com/sites/Marketing/Lists/Product%20List';
-    let results: SearchResults = await this.props.searchService.productSearch(this.state.query, `{searchTerms} ${listUrl}`);    
+    let results: SearchResults = await this.props.searchService.productSearch(this.state.query, `{searchTerms} site:${listUrl}`);    
     this.setState({
       searchResults: results.PrimarySearchResults
     });
